9a2ba0d92537ccdf9c28e377da803d7a1e40b133
Melanie Gunn <mgunn589@gmail.com>
2021-11-12 10:41:45 -0600

    first commit

---
+++ src/components/CardItem.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+const CardItem = () => {
+    return (
+        <div>This is a Card Item</div>
+    )   
+}
+
+export default CardItem;
---
+++ src/components/CardList.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import CardItem from './CardItem';
+
+const CardList = () => {
+    return (
+        <div>
+            <CardItem />
+        </div>
+    );
+}
+
+export default CardList;
