586dc2c625613c2bf773f0973f08518483a477b6
Melanie Gunn <mgunn589@gmail.com>
2021-11-15 09:32:45 -0600

    reduced importing errors.  Still not rendering

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,11 +1,22 @@
 import React, {useState} from 'react';
+import Card from 'react-bootstrap';
+import Button from 'react-bootstrap';
 import './PlaygroundObjects'; 
 
 const CardItem = ({playgroundObjects}) => {
-    
+
     return (
         <div>
-            This is a Card Item
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                <Card.Title>This is a Card Title</Card.Title>
+                <Card.Text>
+                This is Card Text
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
         </div>
     )   
 }
